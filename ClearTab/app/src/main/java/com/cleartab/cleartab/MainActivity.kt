cc4e87145910f209f77c1dcef9c981c6bb545d5e
Nihhiu <106330016+Nihhiu@users.noreply.github.com>
2024-05-05 19:14:46 +0100

    Inicial
    Commit na creação do projeto

---
+++ ClearTab/app/src/main/java/com/cleartab/cleartab/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.cleartab.cleartab
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.cleartab.cleartab.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.navView.setOnItemSelectedListener{
+
+            when(it.itemId){
+
+                R.id.navigation_home -> replaceFragment(Home())
+                R.id.navigation_history -> replaceFragment(History())
+                R.id.navigation_tasks -> replaceFragment(Tasks())
+
+                else -> {
+
+                }
+
+            }
+
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+}
