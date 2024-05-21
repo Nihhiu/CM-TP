34cef483e3c447435f22ff17eab273a607a10828
awlaska <sofiassantossousa@gmail.com>
2024-05-21 15:38:10 +0100

    .

--- ClearTab/app/src/main/java/com/cleartab/cleartab/Slider_Intro.kt
+++ ClearTab/app/src/main/java/com/cleartab/cleartab/Slider_Intro.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+// import android.app.src.main.res.layout.fragment_slider__intro.*
 
 class Slider_Intro : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_slider__intro, container, false)
+    }
+
+    fun setTitle(title: String){
+        slider_text.text = title;
     }
 }
