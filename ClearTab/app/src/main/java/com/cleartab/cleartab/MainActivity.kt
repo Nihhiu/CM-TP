6f67fddcaa24a8a472f28bede883aa369e9d51bd
awlaska <sofiassantossousa@gmail.com>
2024-06-11 09:50:47 +0100

    tentativa

--- ClearTab/app/src/main/java/com/cleartab/cleartab/MainActivity.kt
+++ ClearTab/app/src/main/java/com/cleartab/cleartab/MainActivity.kt
@@ -8,28 +8,45 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.cleartab.cleartab.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewPager: Slider_1
+    private lateinit var tabLayout: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
 
-        val navView: BottomNavigationView = binding.navView
+        viewPager = findViewById(R.id.viewPager)
+        tabLayout = findViewById(R.id.tabLayout)
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_tasks, R.id.navigation_home, R.id.navigation_history
-            )
+        val layoutIds = listOf(
+            R.layout.fragment_slider_1,
+            R.layout.fragment_slider_2,
+            R.layout.fragment_slider_3,
+            R.layout.fragment_slider_4
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        viewPager.adapter = ViewPagerAdapter(this, layoutIds)
+
+        TabLayoutMediator(tabLayout, viewPager) { _, _ -> }.attach()
+
+//        val navView: BottomNavigationView = binding.navView
+//
+//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_tasks, R.id.navigation_home, R.id.navigation_history
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
     }
 }
