bd43001502d7791aba381605703140c75ce0f402
awlaska <sofiassantossousa@gmail.com>
2024-06-19 17:54:53 +0100

    a

--- ClearTab/app/build.gradle.kts
+++ ClearTab/app/build.gradle.kts
@@ -54,4 +54,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("androidx.viewpager2:viewpager2:1.1.0")
     implementation("com.google.android.material:material:1.3.0")
+    implementation("com.android.support.constraint:constraint-layout:2.0.4")
 }
