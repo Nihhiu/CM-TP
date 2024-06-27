517b809863b8d43504385f29ccd6ff98c2d0d9dd
carloscveloso <106328375+carloscveloso@users.noreply.github.com>
2024-06-27 23:44:55 +0100

    Alguns layouts criados

--- ClearTab/app/src/main/java/com/cleartab/cleartab/Slider_Intro.kt
+++ ClearTab/app/src/main/java/com/cleartab/cleartab/Slider_Intro.kt
@@ -12,7 +12,7 @@ import android.widget.TextView
 class Slider_Intro : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_slider__intro, container, false)
+        return inflater.inflate(R.layout.slider__intro, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
