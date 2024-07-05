ccdadb6ed47a49b3eb3b6ed54dbc5afe200771b3
awlaska <sofiassantossousa@gmail.com>
2024-07-05 18:50:34 +0100

    home

---
+++ ClearTab/app/src/main/java/com/cleartab/cleartab/ui/eliminar_projeto/eliminar_projeto.kt
@@ -0,0 +1,7 @@
+package com.cleartab.cleartab.ui.eliminar_projeto
+
+import androidx.appcompat.app.AppCompatActivity
+
+class eliminar_projeto: AppCompatActivity() {
+
+}
--- ClearTab/app/src/main/java/com/cleartab/cleartab/ui/home/home.kt
+++ ClearTab/app/src/main/java/com/cleartab/cleartab/ui/home/home.kt
@@ -12,32 +12,33 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.cleartab.cleartab.R
 import com.cleartab.cleartab.retrofit.SupabaseService
-import com.cleartab.cleartab.ui.criar_projeto.criar_projeto
+import com.cleartab.cleartab.ui.eliminar_projeto.eliminar_projeto
 import com.cleartab.cleartab.ui.projetos.projetosAdapter
 import com.cleartab.cleartab.utils.SharedPreferencesUtil
 import kotlinx.coroutines.launch
 
 class home : AppCompatActivity() {
-    private lateinit var precycler: RecyclerView
-    private lateinit var padicionar: Button
+    private lateinit var heditar: Button
+    private lateinit var hbaixar: Button
+    private lateinit var hmembros: RecyclerView
     private val db = SupabaseService()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.projetos)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.projetos)) { v, insets ->
+        setContentView(R.layout.fragment_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        precycler = findViewById(R.id.recycler)
-        padicionar = findViewById(R.id.button2)
+        heditar = findViewById(R.id.button3)
+        hbaixar = findViewById(R.id.button5)
 
-        precycler.setHasFixedSize(true)
+        hmembros.setHasFixedSize(true)
         val layoutManager = LinearLayoutManager(this)
-        precycler.layoutManager = layoutManager
+        hmembros.layoutManager = layoutManager
 
         // specify an viewAdapter (see also next example)
         lifecycleScope.launch {
@@ -48,11 +49,12 @@ class home : AppCompatActivity() {
                 )
             ) // replace with your data
             val adapter = projetosAdapter(myDataset!!)
-            precycler.adapter = adapter
+            hmembros.adapter = adapter
         }
 
-        padicionar.setOnClickListener {
-            val intent = Intent(this, criar_projeto::class.java)
+        heditar.setOnClickListener {
+            // TODO eliminar projeto
+            val intent = Intent(this, eliminar_projeto::class.java)
             startActivity(intent)
         }
     }
