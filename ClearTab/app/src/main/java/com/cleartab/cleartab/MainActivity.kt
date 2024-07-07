5182429114abe5d69a432d03f3e14f0bf8467166
Nihhiu <106330016+Nihhiu@users.noreply.github.com>
2024-07-07 19:39:32 +0100

    a patria

--- ClearTab/app/src/main/java/com/cleartab/cleartab/MainActivity.kt
+++ ClearTab/app/src/main/java/com/cleartab/cleartab/MainActivity.kt
@@ -1,25 +1,46 @@
 package com.cleartab.cleartab
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.cleartab.cleartab.databinding.ActivityMainBinding
+import com.cleartab.cleartab.retrofit.SupabaseService
+import com.cleartab.cleartab.retrofit.tables.Projeto
+import com.cleartab.cleartab.utils.SharedPreferencesUtil
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var tv_titulo_do_projeto: TextView
+    private lateinit var tv_descricao_do_projeto: TextView
+    private val db = SupabaseService()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        tv_titulo_do_projeto = findViewById(R.id.tv_titulo_do_projeto)
+        tv_descricao_do_projeto = findViewById(R.id.tv_descricao_do_projeto)
+
         val navView: BottomNavigationView = binding.navView
+
+        val idProjeto = SharedPreferencesUtil.getIDs(this@MainActivity, "ID_PROJETO")
+
+        var ProjetoDados: Projeto
+
+        lifecycleScope.launch {
+            ProjetoDados = db.fetchProject(idProjeto = idProjeto)!!
+            tv_titulo_do_projeto.text = ProjetoDados.nome
+            tv_descricao_do_projeto.text = ProjetoDados.descricao
+        }
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
 
